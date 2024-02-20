b710dad42ca7c748725c210786e6e2d8b9b6ed44
MeganHancock <mgnhnck@gmail.com>
2024-02-20 16:15:48 -0700

    posting jobs

--- src/controllers/JobsController.js
+++ src/controllers/JobsController.js
@@ -6,12 +6,28 @@ export class JobsController extends BaseController {
         super('api/jobs')
         this.router
             .get('', this.getJobs)
+            .post('', this.createJob)
     }
 
+    /**
+* @param {import("express").Request} request
+* @param {import("express").Response} response
+* @param {import("express").NextFunction} next
+*/
     async getJobs(request, response, next) {
         try {
             const jobs = await jobsService.getJobs()
             response.send(jobs)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async createJob(request, response, next) {
+        try {
+            const jobData = request.body
+            const createdJob = await jobsService.createJob(jobData)
+            response.send(createdJob)
         } catch (error) {
             next(error)
         }
--- src/services/JobsService.js
+++ src/services/JobsService.js
@@ -3,13 +3,17 @@ import { dbContext } from "../db/DbContext.js"
 class JobsService {
 
 
+
     async getJobs() {
         const jobs = await dbContext.Jobs.find()
         return jobs
 
     }
 
-
+    async createJob(jobData) {
+        const createdJob = dbContext.Jobs.create(jobData)
+        return createdJob
+    }
 }
 
 export const jobsService = new JobsService()
